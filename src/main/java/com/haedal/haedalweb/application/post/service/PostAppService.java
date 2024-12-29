a6630f1be3c5ca1841339dc11b4a644eabf9ff3e
tfer2442 <tfer2442@gmail.com>
2024-12-29 23:42:32 +0900

    feat: 게시글 이미지 등록 Application Service 작성 (#19)

--- src/main/java/com/haedal/haedalweb/application/post/service/PostAppService.java
+++ src/main/java/com/haedal/haedalweb/application/post/service/PostAppService.java
@@ -1,4 +1,7 @@
 package com.haedal.haedalweb.application.post.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface PostAppService {
+    String registerPostImage(MultipartFile postImageFile);
 }
--- src/main/java/com/haedal/haedalweb/application/post/service/PostAppServiceImpl.java
+++ src/main/java/com/haedal/haedalweb/application/post/service/PostAppServiceImpl.java
@@ -1,9 +1,52 @@
 package com.haedal.haedalweb.application.post.service;
 
-import lombok.RequiredArgsConstructor;
+import com.haedal.haedalweb.domain.post.model.PostImage;
+import com.haedal.haedalweb.domain.post.service.PostImageService;
+import com.haedal.haedalweb.domain.user.model.User;
+import com.haedal.haedalweb.infrastructure.image.ImageSaveRollbackEvent;
+import com.haedal.haedalweb.infrastructure.image.ImageUtil;
+import com.haedal.haedalweb.security.service.SecurityService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class PostAppServiceImpl implements PostAppService {
+    private final PostImageService postImageService;
+    private final SecurityService securityService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final String uploadPath;
+    private final String uploadUrl;
+
+    public PostAppServiceImpl(PostImageService postImageService, SecurityService securityService, ApplicationEventPublisher applicationEventPublisher, @Value("${file.path.upload-profile-images}") String uploadPath, @Value("${file.url.upload-profile-images}") String uploadUrl) {
+        this.postImageService = postImageService;
+        this.securityService = securityService;
+        this.applicationEventPublisher = applicationEventPublisher;
+        this.uploadPath = uploadPath;
+        this.uploadUrl = uploadUrl;
+    }
+
+    @Override
+    @Transactional
+    public String registerPostImage(MultipartFile postImageFile) {
+        User loggedInUser = securityService.getLoggedInUser();
+
+        String originalFile = postImageFile.getOriginalFilename();
+        String saveFile = UUID.randomUUID() + "." + ImageUtil.getExtension(originalFile);
+        ImageUtil.uploadImage(postImageFile, uploadPath, saveFile);
+
+        applicationEventPublisher.publishEvent(new ImageSaveRollbackEvent(uploadPath, saveFile));
+
+        PostImage postImage = PostImage.builder()
+                .originalFile(originalFile)
+                .saveFile(saveFile)
+                .user(loggedInUser)
+                .build();
+
+        postImageService.registerPostImage(postImage);
+        return ImageUtil.generateImageUrl(uploadUrl, saveFile);
+    }
 }
