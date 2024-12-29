4944c5865b9dec32257e1a03863f3c6f72f2ed36
tfer2442 <tfer2442@gmail.com>
2024-12-29 22:50:55 +0900

    feat: postImage 등록 service 작성 (#19)

--- src/main/java/com/haedal/haedalweb/domain/post/service/PostImageService.java
+++ src/main/java/com/haedal/haedalweb/domain/post/service/PostImageService.java
@@ -1,4 +1,7 @@
 package com.haedal.haedalweb.domain.post.service;
 
+import com.haedal.haedalweb.domain.post.model.PostImage;
+
 public interface PostImageService {
+    void registerPostImage(PostImage postImage);
 }
--- src/main/java/com/haedal/haedalweb/domain/post/service/PostImageServiceImpl.java
+++ src/main/java/com/haedal/haedalweb/domain/post/service/PostImageServiceImpl.java
@@ -1,9 +1,17 @@
 package com.haedal.haedalweb.domain.post.service;
 
+import com.haedal.haedalweb.domain.post.model.PostImage;
+import com.haedal.haedalweb.domain.post.repository.PostImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostImageServiceImpl implements PostImageService {
+    private final PostImageRepository postImageRepository;
+
+    @Override
+    public void registerPostImage(PostImage postImage) {
+        postImageRepository.save(postImage);
+    }
 }
