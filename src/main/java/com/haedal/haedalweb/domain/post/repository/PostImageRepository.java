3982bcfb69db3bd210cca80ac37f4d62d9bc988d
tfer2442 <tfer2442@gmail.com>
2024-12-29 23:32:15 +0900

    feat: PostId가 존재하지 않는 PostImageCleanup 스케줄러 작성 (#19)

--- src/main/java/com/haedal/haedalweb/domain/post/repository/PostImageRepository.java
+++ src/main/java/com/haedal/haedalweb/domain/post/repository/PostImageRepository.java
@@ -2,8 +2,16 @@ package com.haedal.haedalweb.domain.post.repository;
 
 import com.haedal.haedalweb.domain.post.model.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+    @Query("SELECT pi FROM PostImage pi " +
+            "WHERE pi.post IS NULL " +
+            "AND pi.regDate < :threshold " +
+            "ORDER BY pi.regDate ASC")
+    List<PostImage> findOldImages(LocalDateTime threshold);
 }
