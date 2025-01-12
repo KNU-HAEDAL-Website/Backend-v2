73c40bf39929c77cf88fdc73648189d32e6b743a
tfer2442 <tfer2442@gmail.com>
2025-01-13 00:44:10 +0900

    feat: 댓글 페이징 조회 컨트롤러 작성 (#28)

--- src/main/java/com/haedal/haedalweb/web/comment/controller/CommentController.java
+++ src/main/java/com/haedal/haedalweb/web/comment/controller/CommentController.java
@@ -14,6 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,4 +36,11 @@ public class CommentController {
 
         return ResponseUtil.buildSuccessResponseEntity(SuccessCode.ADD_COMMENT_SUCCESS);
     }
+
+//    @Operation(summary = "댓글 페이징 조회")
+//    @ApiErrorCodeExamples({})
+//    @GetMapping("/posts/{postId}/comments")
+//    public ResponseEntity<?> getComments(@PathVariable Long postId) {
+//        return
+//    }
 }
