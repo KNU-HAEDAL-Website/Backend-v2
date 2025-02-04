22da0e0119c24ef671cbd922ceb7cc42715c28e3
tfer2442 <tfer2442@gmail.com>
2025-02-04 19:06:21 +0900

    feat: 액세스 토큰 만료 시간 1분으로 설정

--- src/main/java/com/haedal/haedalweb/constants/LoginConstants.java
+++ src/main/java/com/haedal/haedalweb/constants/LoginConstants.java
@@ -15,7 +15,8 @@ public final class LoginConstants {
     public static final String ROLE_CLAIM = "role";
     public static final String CATEGORY_CLAIM = "category";
 
-    public static final long ACCESS_TOKEN_EXPIRATION_TIME_MS = 7*24*60*60*1000; // dev 7 days
+//    public static final long ACCESS_TOKEN_EXPIRATION_TIME_MS = 7*24*60*60*1000; // dev 7 days
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME_MS = 60*1000; // 1분
     public static final long REFRESH_TOKEN_EXPIRATION_TIME_MS = 86400*1000; // 1 day
     public static final long REFRESH_TOKEN_EXPIRATION_TIME_S = 86400; // 1day
 }
