cd097a1aa9cc5763ae78bc00b113b1a2c045061b
tfer2442 <tfer2442@gmail.com>
2025-02-23 17:18:41 +0900

    style: check style에 따라 BoardRepository 스타일 수정 (#55)

--- src/main/java/com/haedal/haedalweb/domain/board/repository/BoardRepository.java
+++ src/main/java/com/haedal/haedalweb/domain/board/repository/BoardRepository.java
@@ -16,35 +16,43 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 	boolean existsByActivityId(Long activityId);
 
 	@Query(
-		value = "SELECT b FROM Board b " +
-			"JOIN FETCH b.boardImage " + // 여기서 OneToOne 연관 엔티티 fetch join
-			"JOIN FETCH b.user " +
-			"WHERE b.activity.id = :activityId",
+		value = "SELECT b FROM Board b "
+			+ "JOIN FETCH b.boardImage " // 여기서 OneToOne 연관 엔티티 fetch join
+			+ "JOIN FETCH b.user "
+			+ "WHERE b.activity.id = :activityId",
 		countQuery = "SELECT count(b) FROM Board b WHERE b.activity.id = :activityId"
 	)
 	Page<Board> findBoardPage(@Param("activityId") Long activityId, Pageable pageable);
 
-	@Query("SELECT DISTINCT b FROM Board b " +
-		"JOIN FETCH b.boardImage " +
-		"WHERE b.id = :boardId")
+	@Query(
+		"SELECT DISTINCT b FROM Board b "
+			+ "JOIN FETCH b.boardImage "
+			+ "WHERE b.id = :boardId"
+	)
 	Optional<Board> findBoard(Long boardId);
 
-	@Query("SELECT DISTINCT b FROM Board b " +
-		"JOIN FETCH b.user " +
-		"JOIN FETCH b.boardImage " +
-		"WHERE b.id = :boardId AND b.activity.id = :activityId")
+	@Query(
+		"SELECT DISTINCT b FROM Board b "
+			+ "JOIN FETCH b.user "
+			+ "JOIN FETCH b.boardImage "
+			+ "WHERE b.id = :boardId AND b.activity.id = :activityId"
+	)
 	Optional<Board> findBoardWithUserAndParticipants(Long activityId, Long boardId);
 
-	@Query("SELECT DISTINCT b FROM Board b " +
-		"JOIN FETCH b.boardImage " +
-		"JOIN FETCH b.participants " +
-		"JOIN FETCH b.user " +
-		"WHERE b.id = :boardId AND b.activity.id = :activityId")
+	@Query(
+		"SELECT DISTINCT b FROM Board b "
+			+ "JOIN FETCH b.boardImage "
+			+ "JOIN FETCH b.participants "
+			+ "JOIN FETCH b.user "
+			+ "WHERE b.id = :boardId AND b.activity.id = :activityId"
+	)
 	Optional<Board> findBoardWithParticipants(Long activityId, Long boardId);
 
-	@Query("SELECT DISTINCT b FROM Board b " +
-		"JOIN FETCH b.boardImage " +
-		"JOIN FETCH b.user " +
-		"WHERE b.id = :boardId AND b.activity.id = :activityId")
+	@Query(
+		"SELECT DISTINCT b FROM Board b "
+			+ "JOIN FETCH b.boardImage "
+			+ "JOIN FETCH b.user "
+			+ "WHERE b.id = :boardId AND b.activity.id = :activityId"
+	)
 	Optional<Board> findBoardWithUser(Long activityId, Long boardId);
 }
