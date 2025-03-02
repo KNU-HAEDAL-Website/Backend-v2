4dd6509e11714de33a8a93bd68e24ffd20ddd59f
tfer2442 <tfer2442@gmail.com>
2025-03-02 18:21:40 +0900

    feat: ActivityRepository 인터페이스와 구현체 Layer 분리 및 Adapter 패턴 적용 (#50)

--- src/main/java/com/haedal/haedalweb/domain/activity/repository/ActivityRepository.java
+++ src/main/java/com/haedal/haedalweb/domain/activity/repository/ActivityRepository.java
@@ -3,16 +3,19 @@ package com.haedal.haedalweb.domain.activity.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.haedal.haedalweb.domain.activity.model.Activity;
 
-@Repository
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
+public interface ActivityRepository {
+	Optional<Activity> findById(Long activityId);
+
 	Optional<Activity> findBySemesterIdAndId(Long semesterId, Long activityId);
 
 	List<Activity> findBySemesterId(Long semesterId);
 
 	boolean existsBySemesterId(Long semesterId);
+
+	Activity save(Activity activity);
+
+	void delete(Activity activity);
 }
+
---
+++ src/main/java/com/haedal/haedalweb/infrastructure/activity/repository/ActivityJpaRepository.java
@@ -0,0 +1,18 @@
+package com.haedal.haedalweb.infrastructure.activity.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.haedal.haedalweb.domain.activity.model.Activity;
+
+@Repository
+public interface ActivityJpaRepository extends JpaRepository<Activity, Long> {
+	Optional<Activity> findBySemesterIdAndId(Long semesterId, Long activityId);
+
+	List<Activity> findBySemesterId(Long semesterId);
+
+	boolean existsBySemesterId(Long semesterId);
+}
