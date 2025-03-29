1f11ace3ece0ee39a08a8969c77029074132a75d
yongminKwon1 <ym.kwon@lipis.kr>
2025-03-30 03:24:04 +0900

    fix: JoinRequestDto의 joinedSemesterId 검증 애너테이션을 @NotBlank에서 @NotNull로 수정

--- src/main/java/com/haedal/haedalweb/application/user/dto/JoinRequestDto.java
+++ src/main/java/com/haedal/haedalweb/application/user/dto/JoinRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -41,6 +42,6 @@ public class JoinRequestDto {
 	private String userName;
 
 	@Schema(description = "가입 학기", example = "2025-1")
-	@NotBlank(message = "가입 학기는 필수 입력 항목입니다.")
+	@NotNull(message = "가입 학기는 필수 입력 항목입니다.")
 	private Long joinedSemesterId;
 }
